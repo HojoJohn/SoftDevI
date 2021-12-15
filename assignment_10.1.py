4386b629f923068f8d461bbef5813d7a37b72bad
JonathanLopez42 <jll3888@rit.edu>
2021-12-14 22:09:23 -0500

    sup

--- assignment_10.1.py
+++ assignment_10.1.py
@@ -1,3 +1,5 @@
+import random
+
 def fusion(a_list):
     sorted_list = sorted(a_list)
     copy = []
@@ -19,6 +21,22 @@ def fusion(a_list):
 
 def create_particles(max_coordinates):
 
-    x = []
-    y = []
-    z = []
+    sety = set()
+    n = max_coordinates * 3
+
+    for  i in range(0, n):
+        x = random.randint(0, max_coordinates)
+        y = random.randint(0, max_coordinates)
+        z = random.randint(0, max_coordinates)
+        coords = (x, y, z)
+
+        sety.add(coords)
+    
+    return sety
+
+def build_dict_xy(a_set):
+    a_set = [x,y]
+    for i in range(0, len(a_set[i])):
+        return tuple[x,y,z]
+
+        
